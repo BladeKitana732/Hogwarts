6feef08ac21af96b2b8845f593920ca556ff6290
Teresa Malave <dev.malave7@gmail.com>
2020-05-15 10:51:17 -0500

    set foundation of app for everyone in group with router/navbar/ and components to be worked on

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Login() {
+    return (
+        <div>
+            <h1>Sanity check Login component</h1>
+        </div>
+    )
+}
---
+++ src/components/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Profile() {
+    return (
+        <div>
+            <h1>Sanity check Profile component</h1>
+        </div>
+    )
+}
