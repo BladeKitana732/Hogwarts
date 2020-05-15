39ea4d028f03bdb4b51c03b61c0721529a7e65cf
Teresa Malave <dev.malave7@gmail.com>
2020-05-15 15:17:48 -0500

    adding content to page/not rendering api yet

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,11 @@ import React from 'react';
 export default function Home() {
     return (
         <div>
-            <h1>Sanity check Home component</h1>
-            <p>branch test</p>
+            <h1>Welcome to our webpage!</h1>
+            <h3>Why are you here?</h3>
+            <p>
+            You have landed where we make finding everything about your favorite superhero and/or villain as easy as breathing.  You got here by signing up or logging back in! Thanks for supporting our website to keep bringing you nothing but the best. 
+            </p>
         </div>
     )
 }
