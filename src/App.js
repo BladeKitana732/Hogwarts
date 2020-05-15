4f086645b259d50842832267ea4e5b098a826ce6
Student_M14 <student_m14@PS-DAL-M14.home>
2020-05-15 13:33:22 -0500

    blueprint of hero

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Profile from './components/Profile';
 import Hero from './components/Hero';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import React from './heroComp/hero';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Route exact path ="/" component= {Login} />
-      <Route exact path ="/Home" component= {Home} />
-      <Route exact path ="/Profile" component= {Profile} />
-      <Route exact path ="/Hero" component= {Hero} />
-    </Router>
+    <div>
+
+    </div>
+    // <Router>
+    //   <Navbar />
+    //   <Route exact path ="/" component= {Login} />
+    //   <Route exact path ="/Home" component= {Home} />
+    //   <Route exact path ="/Profile" component= {Profile} />
+    //   <Route exact path ="/Hero" component= {Hero} />
+    // </Router>
   );
 }
 
