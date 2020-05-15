05c911a8f44174bfa0bf62abc665f959553023b5
Teresa Malave <dev.malave7@gmail.com>
2020-05-15 18:59:09 -0500

    had to change Home component to class. figuring out how to render the images of some of the heros/villains in api data. commented how api works

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default function Home() {
+
+let apiLink = axios.create({
+    //using id as base url for this api needs an id number before rendering any data from api itself.
+    baseURL: "https://superheroapi.com/api/682168042639893/id"
+})
+
+
+
+export default class Home extends Component() {
+
+    state = {
+        
+        heroImage: []
+
+    }
+
+    constructor(props){
+        super(props);
+
+
+
+        
+    }
+
+
+
+
+
+
+
+
+
     return (
         <div>
             <h1>Welcome to our webpage!</h1>
@@ -10,6 +42,8 @@ export default function Home() {
             <p>
             You have landed where we make finding everything about your favorite superhero and/or villain as easy as breathing.  You got here by signing up or logging back in! Thanks for supporting our website to keep bringing you nothing but the best. 
             </p>
+
+
         </div>
     )
 }
