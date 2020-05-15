ff7d7b4e6bbdf8dafbafa14d5eac0add0447474a
dev.rayavaram <dev.rayavaram@gmail.com>
2020-05-15 15:36:17 -0500

    added login functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import './App.css';
 import Login from './components/Login';
 import Home from './components/Home';
@@ -6,17 +6,49 @@ import Profile from './components/Profile';
 import Hero from './components/Hero';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import firebase  from './config/fireauth';
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Route exact path ="/" component= {Login} />
-      <Route exact path ="/Home" component= {Home} />
-      <Route exact path ="/Profile" component= {Profile} />
-      <Route exact path ="/Hero" component= {Hero} />
-    </Router>
-  );
+class  App extends Component{
+  constructor(props){
+    super();
+    this.state={
+      user:{}
+    }
+  }
+  componentDidMount(){
+    this.authListener();
+  }
+  authListener(){
+    firebase.auth().onAuthStateChanged((user)=>{
+      //console.log(user);
+      if(user){
+        this.setState({user});
+      }
+      else{
+        this.setState({user:null});
+      }
+    })
+  }
+  render(){
+        return (
+          this.state.user?(
+            <div className="App">
+              <Router>
+                <Navbar />
+                    <Route exact path ="/Home" component= {Home} />
+                    <Route exact path ="/Profile" component= {Profile} />
+                    <Route exact path ="/Hero" component= {Hero} />
+                </Router>          
+            </div>
+          ):(
+              <div className="App">
+                <Login/>
+              </div>
+
+          )
+         );
+  }
+ 
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,91 @@
-import React from 'react';
+import React ,{Component } from 'react';
+import firebase from '../config/fireauth'
+import { Button } from 'react-bootstrap';
+/*
+    create class component Login
+        initialize user state
+        bind handleSubmit,handleSubmit,and handleChange methods to class
+        procedure handleChange
+            get user input 
+            set user state
+        procedure handleSubmit
+            get user state
+            call firebase.auth by passing user information
+            IF login is successful set uid and other user info
+            ELSE alert user to enter correct info or signup
+        procedure handleSignup
+            create user in firebase with email/password
+            get uid and set user state
+*/
 
-export default function Login() {
-    return (
-        <div>
-            <h1>Sanity check Login component</h1>
+class  Login extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            email:'',
+            password:''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSignup = this.handleSignup.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+
+    }
+    handleSignup(e){
+         e.preventDefault();
+         firebase.auth().createUserWithEmailAndPassword(this.state.email,this.state.password).then(
+             (u)=>{
+                    console.log(u)
+                    console.log("u.uid",u.user.uid)
+              }
+         ).catch(e=>{
+             console.log(e)
+         })
+    }
+    handleChange(e){
+        this.setState({[e.target.name]:e.target.value})
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        firebase.auth().signInWithEmailAndPassword(this.state.email,this.state.password).then(
+            (u)=>{
+                console.log("result",u)
+                if(!("code" in u)== null){
+                    console.log("inside if")
+                }
+            }
+        ).catch(e=>{
+            console.log(e)
+            console.log("error",e)
+            alert("User Not found !!! Please enter valid data or Signup");
+           
+        })
+
+    }
+    render(){
+      return (
+        <div className="container">
+            <form>
+                <div className="col-8">
+                    <label className="label" >Email:</label>
+                    <input  className="input" value={this.state.email} onChange={this.handleChange} type="email" name="email"
+                    id="EmailId" />
+                </div>
+                <div className="col-8">
+                    <label >Password:</label>
+                    <input className="input"  value={this.state.password} onChange={this.handleChange} type="password" name="password"
+                    id="PasswordId" />
+                </div>
+                <div className="col-8">
+                    <Button variant="primary" type="submit" onClick={this.handleSubmit}> Login</Button>
+                    <Button variant="success" onClick={this.handleSignup}> Register</Button>
+                </div>
+
+            </form>
+            <label></label>
         </div>
-    )
-}
+      );
+    }
+   
+  }
+  
+  export default Login;
