3871388fb5eb74bc338da4712cdf632368434fee
Teresa Malave <dev.malave7@gmail.com>
2020-05-16 17:52:58 -0500

    got sorting hat of api to render the random house! :D progress

--- src/components/Wizard.js
+++ src/components/Wizard.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+//referencing wine api work
+
+let hogwarts = axios.create({
+    baseURL: "https://www.potterapi.com/v1/sortingHat"
+})
+
+
+
 export default class Wizard extends Component {
+
+    state = {
+        house: []
+    }
+
+    constructor(){
+        super();
+
+        hogwarts.get('/') 
+
+        .then((result) => {
+            let info = result.data;
+
+            console.log(info)
+            
+        })
+    }
+
     render() {
         return (
             <div>
