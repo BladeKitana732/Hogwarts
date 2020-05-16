2893f9cadd5e120c3b6850f32e76b17f36103b94
Teresa Malave <dev.malave7@gmail.com>
2020-05-16 18:51:13 -0500

    changed variable name. now giving me undefined in console. closer :D

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Wizard from './components/Wizard';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 
-
+console.log(process.env.REACT_APP_KEY_VARIABLE);
 
 function App() {
   return (
