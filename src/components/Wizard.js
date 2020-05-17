e786387e2a97b53893802657329774ee2d9a4b7f
Teresa Malave <dev.malave7@gmail.com>
2020-05-16 22:38:06 -0500

    yayy progress thanks to help

--- src/components/Wizard.js
+++ src/components/Wizard.js
@@ -2,39 +2,44 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 console.log("wizard comp");
-console.log(process.env.REACT_APP_KEY);
+// console.log(process.env.REACT_APP_KEY);
+
+const key = process.env.REACT_APP_KEY;
+
 //referencing async/try/catch
 
+console.log(`https://www.potterapi.com/v1/characters?key=${key}`);
+
 export default class Wizard extends Component {
-    constructor(props) {
-		super(props);
-		this.state = {
-			info: []
-		};
-	}
+    // constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	// 		info: []
+	// 	};
+	// }
  
-    async allInfo() {
-		try {
-			const allData = await axios.get(
-				`https://www.potterapi.com/v1/characters?key=process.env.REACT_APP_KEY`
-			);
+    // async allInfo() {
+	// 	try {
+	// 		const allData = await axios.get(
+	// 			`https://www.potterapi.com/v1/characters?key=process.env.REACT_APP_KEY`
+	// 		);
             
-            console.log(allData);
-            console.log(allData.data);
+    //         console.log(allData);
+    //         console.log(allData.data);
             
 
-			this.setState({
-				info: allData.data,
-            });
+	// 		this.setState({
+	// 			info: allData.data,
+    //         });
             
-		} catch (error) {
-			console.log(error);
-		}
-	}
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// }
 
-    componentDidMount() {
-		this.allInfo();
-	}
+    // componentDidMount() {
+	// 	this.allInfo();
+	// }
 
     
     render() {
