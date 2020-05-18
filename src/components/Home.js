e3ddc71d9255e7f507aa42ce0869b0efb77f601f
Teresa Malave <dev.malave7@gmail.com>
2020-05-18 15:02:35 -0500

    added to home component! user can click on sorting hat img to render their house randomly

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import sortingHatImg from './sortingHatImg.jpeg';
 
 const sortingHat = 'https://www.potterapi.com/v1/sortingHat';
 
@@ -38,7 +39,10 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <button onClick = {this.getHouse}>Click me</button>
+                <h1>Click sorting hat to get your house!</h1>
+                <img src= {sortingHatImg} alt= "sorting hat" onClick = {this.getHouse} style = {{width:400, height:400}}/>
+
+                {/* <button onClick = {this.getHouse}>Click me</button> */}
                 <h1><strong>Your House is:</strong>{this.state.house}</h1>
             </div>
         )
