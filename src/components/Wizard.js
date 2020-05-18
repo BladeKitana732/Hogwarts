bd69de4247c384412b2eebf10df984b35a47abee
Teresa Malave <dev.malave7@gmail.com>
2020-05-17 23:07:32 -0500

    adding foundation of how page should render info by user text being input into input box

--- src/components/Wizard.js
+++ src/components/Wizard.js
@@ -13,10 +13,20 @@ const key = process.env.REACT_APP_KEY;
 const workingLink = `https://www.potterapi.com/v1/characters?key=${key}`
 
 export default class Wizard extends Component {
+    constructor(props){
+        super(props)
 
-    state = {
-        info: []
+        //this is supposed to take input of user when searching name and/or house and render back all info of that witch/wizard OR when typing in house to return all names of witches/wizards who belong to said house
+        this.state = {
+            input: '',
+            infoBack: []
+        }
+
+
+
     }
+
+ 
     
  componentDidMount() {
 
@@ -45,13 +55,30 @@ export default class Wizard extends Component {
     render() {
         return (
             <div>
-                <h1>Wizard info Component</h1>
+                <form onSubmit={this.handlingSubmit}>
+                    <label>
+                        Please enter name of witch or wizard. You may also type in the house to see all who belong to it.
 
-                {this.state.info.map(details =>
-                <div key= { details.id }><h2>{details.name}</h2>
+                        <input type = "text" value={this.state.infoBack} onChange = {this.userInput} />
+
+                    </label>
+                    
+                    <input type="submit" value="Submit" />
+                </form>
+
+
+
+                {/* {this.state.info.map(details =>
+                <div key= { details.id }>
+
+                <h2>{details.name}</h2>
+                
+                <h2>{details.house}</h2>
+                
+
                 </div> 
                 
-                )}
+                )} */}
             </div>
         )
     }
