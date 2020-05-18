69059deb9a557ff342aebfe91dddf539ffbdec4d
Student_M14 <student_m14@PS-DAL-M14.home>
2020-05-18 10:21:50 -0500

    integrated api

--- src/components/Spells.js
+++ src/components/Spells.js
@@ -1,11 +1,38 @@
 import React,{Component} from 'react';
-import 'axios';
+import axios from 'axios';
 
+
+const key = process.env.REACT_APP_API_KEY
+
+const spells = `https://www.potterapi.com/v1/spells?key=${key}`
 
 export default class Spells extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            input: '',
+            infoback:[]
+        }
+    }
+
+    componentDidMount() {
+        axios.get(spells)
+
+        .then((result) => {
+            const allData = result.data;
+            
+            this.setState({
+                infoBack:allData
+            })
+        })
+        .catch((err) => {
+            console.log('error', err)
+        })
+    }
 
     render() {
-        console.log(process.env.REACT_APP_API_KEY)
+        
         return(
             
             <React.Fragment>
